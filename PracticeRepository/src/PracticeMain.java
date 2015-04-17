a3733f2f58bf544edf3e12ffadf8e6a36fc44949
patelsweta <patelsweta645@gmail.com>
2015-04-17 14:40:44 -0700

    Commit PracticeMain.java

--- PracticeRepository/src/PracticeMain.java
+++ PracticeRepository/src/PracticeMain.java
@@ -8,6 +8,7 @@ public class PracticeMain {
 		System.out.println("Richard Cross = rkjc");
 		// added this additional comment
 
+		System.out.println("Sweta Patel = patelsweta");
 		//added this on branch b1
 	}
 
