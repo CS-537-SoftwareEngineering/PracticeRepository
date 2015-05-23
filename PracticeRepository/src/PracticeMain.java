9ad860b4948459db7e635e783a1746257efc3eea
nirajpatel2402 <nirajpatel2402@gmail.com>
2015-05-23 10:17:58 -0700

    testing

--- PracticeRepository/src/PracticeMain.java
+++ PracticeRepository/src/PracticeMain.java
@@ -3,11 +3,11 @@ public class PracticeMain {
 
 	public static void main(String[] args) {
 		// every one needs to add a line of code to this program to 
-		//print out your name and github username
+		//print out your name and github username tests
 		
 		System.out.println("Richard Cross = rkjc");
 		
-  		System.out.println("Niraj Patel = nirajpatel2402");   
+  		System.out.println("Niraj Patel = nirajpatel2402");    
 	
 	}
 
